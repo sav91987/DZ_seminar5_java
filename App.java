62c97c24d1ea39c5d28910ec2aec4ab8ad1f2b26
Sotnikov_Andrey <sav91987@ya.ru>
2023-06-22 15:54:35 +0300

    added task2

--- App.java
+++ App.java
@@ -10,15 +10,15 @@ public class App {
 
         HashMap<Integer, String> idAndSurname = new HashMap<>(); // Заполняем Map id (Чтобы избежать повторов фамилий) +
                                                                  // фамилия
-        idAndSurname.put(1, "Иванов");                 // Данный Map будет Key для сборки итогового Map
+        idAndSurname.put(1, "Иванов"); // Данный Map будет Key для сборки итогового Map
         idAndSurname.put(2, "Иванов");
         idAndSurname.put(3, "Иванов");
         idAndSurname.put(4, "Петров");
         idAndSurname.put(5, "Сидоров");
         idAndSurname.put(6, "Холодов");
 
-        ArrayList<ArrayList<Integer>> phones = new ArrayList<>();   // Заполняем List List`ами из телефонов.
-        phones.add(new ArrayList<>(List.of(111, 112, 113)));        // Он будет Value для сборки итогового Map
+        ArrayList<ArrayList<Integer>> phones = new ArrayList<>(); // Заполняем List List`ами из телефонов.
+        phones.add(new ArrayList<>(List.of(111, 112, 113))); // Он будет Value для сборки итогового Map
         phones.add(new ArrayList<>(List.of(221, 222)));
         phones.add(new ArrayList<>(List.of(331, 332, 333, 444)));
         phones.add(new ArrayList<>(List.of(444)));
@@ -27,7 +27,25 @@ public class App {
 
         Solutions.task1.fillAndPrintPhoneBook(idAndSurname, phones);
 
-
         System.out.println("Task2");
+
+        ArrayList<Integer> arr = Solutions.task2.fillArr();
+        int num = arr.size();
+
+        // Создание кучи
+        for (int i = num / 2 - 1; i >= 0; i--) {
+            Solutions.task2.HeapSort(arr, num, i);
+        }
+
+        // Сортировка кучи
+        for (int i = num - 1; i >= 0; i--) {
+            // Замена первого (максимального) элемента с последним
+            int temp = arr.get(0);
+            arr.set(0, arr.get(i));
+            arr.set(i, temp);
+            // Сортировка на уменьшеной куче
+            Solutions.task2.HeapSort(arr, i, 0);
+        }
+        System.out.println(arr.toString());
     }
 }
---
+++ Solutions/task2.java
@@ -0,0 +1,44 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class task2 {
+    // Наполняем лист элементами
+    public static ArrayList<Integer> fillArr() {
+
+        ArrayList<Integer> mass = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            mass.add(rand.nextInt(10));
+        }
+        System.out.println(mass.toString());
+        return mass;
+    }
+
+    // Алгорим сортировки
+    public static void HeapSort(ArrayList<Integer> mass, int num, int i) {
+
+        int rootIndex = i; // индекс родительского элемента
+        int leftIndex = 2 * i + 1; // индекс левого дочернего элемента
+        int rightIndex = 2 * i + 2; // индекс правого дочернего элемента
+
+        // Сравнение трех элементов: родителя и двух дочерних и нахождение большего
+        // и проверка на то, что индексы левый и правый не выходят за пределы списка
+        if (leftIndex < num && mass.get(leftIndex) > mass.get(rootIndex)) {
+            rootIndex = leftIndex;
+        }
+        if (rightIndex < num && mass.get(rightIndex) > mass.get(rootIndex)) {
+            rootIndex = rightIndex;
+        }
+        // Если индекс родителя не совпал с индексом наибольшего из трех элементов, то
+        // идет смена родителя
+        if (rootIndex != i) {
+            int temp = mass.get(i);
+            mass.set(i, mass.get(rootIndex));
+            mass.set(rootIndex, temp);
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            HeapSort(mass, num, rootIndex);
+        }
+    }
+}
